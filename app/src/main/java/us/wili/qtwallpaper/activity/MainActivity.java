2b146e9ffcab2f2565e2e7088a8e933f84805a4f
niorgai <jianqiu.sysu@gmail.com>
2015-12-21 00:16:14 +0800

    add behavior

--- app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
+++ app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package us.wili.qtwallpaper;
+package us.wili.qtwallpaper.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import us.wili.qtwallpaper.R;
 
 public class MainActivity extends AppCompatActivity {
 

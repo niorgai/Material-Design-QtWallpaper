cbf38320a4d64b1019295db827fef8b783cceac4
niorgai <jianqiu.sysu@gmail.com>
2015-12-30 01:52:41 +0800

    change to drawerLayout

---
+++ app/src/main/java/us/wili/qtwallpaper/activity/BaseActivity.java
@@ -0,0 +1,7 @@
+package us.wili.qtwallpaper.activity;
+
+/**
+ * Created by qiu on 12/29/15.
+ */
+public class BaseActivity {
+}
--- app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
+++ app/src/main/java/us/wili/qtwallpaper/activity/MainActivity.java
@@ -1,15 +1,65 @@
 package us.wili.qtwallpaper.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import us.wili.qtwallpaper.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int PAGE_HOT = 0;
+    public static final int PAGE_CATEGORY = 1;
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initViews();
+
+        initData();
+    }
+
+    private void initViews() {
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.hot);
+        }
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    private void initData() {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

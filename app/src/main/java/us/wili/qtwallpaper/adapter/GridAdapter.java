ec124e86965dc6a3fc83fae6f517f08649aede4c
niorgai <jianqiu.sysu@gmail.com>
2016-01-05 00:36:17 +0800

    reuse Hierarchy

--- app/src/main/java/us/wili/qtwallpaper/adapter/GridAdapter.java
+++ app/src/main/java/us/wili/qtwallpaper/adapter/GridAdapter.java
@@ -1,22 +1,27 @@
 package us.wili.qtwallpaper.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import us.wili.qtwallpaper.R;
-import us.wili.qtwallpaper.global.QTApplication;
+import us.wili.qtwallpaper.utils.PictureUtils;
 
 /**
  * Common Grid Adapter
  * item's mask is 280 * 500 in 2 cols.
  * Created by qiu on 1/3/16.
  */
-public class GridAdapter extends RecyclerView.Adapter {
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.GridViewHolder> {
 
     public static final int TYPE_LEFT = 0;
     public static final int TYPE_RIGHT = 1;
@@ -35,7 +40,7 @@ public class GridAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public GridAdapter.GridViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == TYPE_LEFT) {
             return new GridViewHolder(mInflater.inflate(R.layout.item_grid_left, parent, false));
         } else {
@@ -44,13 +49,13 @@ public class GridAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(GridViewHolder holder, int position) {
+        holder.mSimpleDraweeView.getHierarchy().setPlaceholderImage(new ColorDrawable(PictureUtils.getRandomColor(mContext)));
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 100;
     }
 
     class GridViewHolder extends RecyclerView.ViewHolder {
@@ -60,9 +65,14 @@ public class GridAdapter extends RecyclerView.Adapter {
             super(itemView);
             mSimpleDraweeView = (SimpleDraweeView) itemView.findViewById(R.id.drawee_view);
             mSimpleDraweeView.setAspectRatio(0.56f);
-            QTApplication application = (QTApplication) mContext.getApplicationContext();
             //set random loading color
-            mSimpleDraweeView.setHierarchy(application.getRandomBuilder());
+            DraweeController controller = Fresco.newDraweeControllerBuilder().setOldController(mSimpleDraweeView.getController()).build();
+            GenericDraweeHierarchy draweeHierarchy = mSimpleDraweeView.getHierarchy();
+            if (draweeHierarchy == null) {
+                draweeHierarchy = new GenericDraweeHierarchyBuilder(mContext.getResources()).setFadeDuration(400).build();
+            }
+            controller.setHierarchy(draweeHierarchy);
+            mSimpleDraweeView.setController(controller);
         }
     }
 }
--- app/src/main/java/us/wili/qtwallpaper/utils/PictureUtils.java
+++ app/src/main/java/us/wili/qtwallpaper/utils/PictureUtils.java
@@ -1,9 +1,37 @@
 package us.wili.qtwallpaper.utils;
 
+import android.content.Context;
+
+import java.util.Random;
+
+import us.wili.qtwallpaper.R;
+
 /**
  * use for picture load
  * Created by qiu on 1/3/16.
  */
 public class PictureUtils {
 
+    public static int getRandomColor(Context context) {
+        int randomColor = new Random().nextInt() % 8;
+        switch (randomColor) {
+            case 0:
+                return context.getResources().getColor(R.color.holder1);
+            case 1:
+                return context.getResources().getColor(R.color.holder2);
+            case 2:
+                return context.getResources().getColor(R.color.holder3);
+            case 3:
+                return context.getResources().getColor(R.color.holder4);
+            case 4:
+                return context.getResources().getColor(R.color.holder5);
+            case 5:
+                return context.getResources().getColor(R.color.holder6);
+            case 6:
+                return context.getResources().getColor(R.color.holder7);
+            default:
+                return context.getResources().getColor(R.color.holder8);
+
+        }
+    }
 }
